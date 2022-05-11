c8523ea4bafb0aa3e6d8c7f750a6e56012205267
Jonathan Lafleur <me@jonathanlafleur.ca>
2022-05-11 01:24:36 -0400

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPage } from './pages/contact/contact.page';
+import { HomePage } from './pages/home/home.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  },
+  {
+    path: 'contact',
+    component: ContactPage
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogModule } from './blog/blog.module';
+import { HomePage } from './pages/home/home.page';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxTypedJsModule } from 'ngx-typed-js';
+import { CtaComponent } from './components/cta/cta.component';
+import { ContactPage } from './pages/contact/contact.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePage,
+    ContactPage,
+    NavigationComponent,
+    CtaComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    BlogModule,
+    FontAwesomeModule,
+    NgxTypedJsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
